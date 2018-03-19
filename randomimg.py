0808421493cde607cce12e31b94d4b8724fb579d
oookr <32430440+oookr@users.noreply.github.com>
2018-03-19 22:33:57 +0300

    Upgrade randomimg.py

--- randomimg.py
+++ randomimg.py
@@ -3,33 +3,64 @@ import random as r
 print ("How many numbers?")
 num = int(input ())
 if num > 100:
-		print ("asdasdsd >  100")
+	print ("Number >  100, enter the number < 100 ")
+	while num > 100 :
+		if num > 100:
+		#	print ("Number >  100, enter the number < 100 ")
+			print ("How many numbers?")
+		num = int(input ())
+		if num <= 100:
+			for i in range(num):	
+				print( r.randint(0, 1))
+	pass
 elif num <= 100:
 	for i in range(num):	
 		print( r.randint(0, 1))
+
+
 tet = str(input("Do you want to have fun? (y or n) "))
 if tet == "n":
 	print(':(')
-elif  tet == "y" "":
-
-	for i in range(9999999999):	
+elif  tet == "y":
+	for i in range():	
 		print( r.randint(0, 1))
 else:
-	while tet != "n":
-		tet = str(input("Do you want to have fun? (y or n) "))
-		if tet == "n":
-			print(':(')
-		elif  tet == "y" "":
-			for i in range(9999999999):	
-				print( r.randint(0, 1))
 	while tet != "y":
-		# pass
-		tet = str(input("Do you want to have fun? (y or n) "))
-		if tet == "n":
-			print(':(')
-		elif  tet == "y" "":
-			for i in range(9999999999):	
-				print( r.randint(0, 1))
+		while tet != "n":
+			
+			tet = str(input("Do you want to have fun? (y or n) "))
+			if tet == "n":
+				print(':(')
+			elif  tet == "y":
+				for i in range(): #!!!STOP program "Ctrl + C"!!!	#range ()-infinitely ; range (1) - one
+					print( r.randint(0, 1))
+		pass	
+	pass
+
+# tet = str(input("Do you want to have fun? (y or n) "))
+# if tet == "n":
+# 	print(':(')
+# elif  tet == "y" "":
+
+# 	for i in range(9999999999):	
+# 		print( r.randint(0, 1))
+# else:
+# 	while tet != "n":
+# 		tet = str(input("Do you want to have fun? (y or n) "))
+# 		if tet == "n":
+# 			print(':(')
+# 		elif  tet == "y" "":
+# 			for i in range(9999999999):	
+# 				print( r.randint(0, 1))
+# 	while tet != "y":
+# 		# pass
+# 		tet = str(input("Do you want to have fun? (y or n) "))
+# 		if tet == "n":
+# 			print(':(')
+# 		elif  tet == "y" "":
+# 			for i in range(9999999999):	
+# 				print( r.randint(0, 1))
+
 	# tet = str( input( "Do you want to have fun? ( y or n) "))
 	# if tet == "n":
 	# 	print(':(')
@@ -37,4 +68,4 @@ else:
 	# 	for i in range(9999999999):	
 	# 		print( random.randint(0, 1))
 	# else:
-	# 	print("FuCk you!!!")
+	# 	print("oh noo!!!")
