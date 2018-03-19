34faa11e1841ffc6c8ad3d125fcc477f39d438af
oookr <32430440+oookr@users.noreply.github.com>
2018-03-19 18:34:46 +0300

    Update .gitignore, testind.py and randomimg.py

---
+++ randomimg.py
@@ -0,0 +1,40 @@
+import random as r
+ 
+print ("How many numbers?")
+num = int(input ())
+if num > 100:
+		print ("asdasdsd >  100")
+elif num <= 100:
+	for i in range(num):	
+		print( r.randint(0, 1))
+tet = str(input("Do you want to have fun? (y or n) "))
+if tet == "n":
+	print(':(')
+elif  tet == "y" "":
+
+	for i in range(9999999999):	
+		print( r.randint(0, 1))
+else:
+	while tet != "n":
+		tet = str(input("Do you want to have fun? (y or n) "))
+		if tet == "n":
+			print(':(')
+		elif  tet == "y" "":
+			for i in range(9999999999):	
+				print( r.randint(0, 1))
+	while tet != "y":
+		# pass
+		tet = str(input("Do you want to have fun? (y or n) "))
+		if tet == "n":
+			print(':(')
+		elif  tet == "y" "":
+			for i in range(9999999999):	
+				print( r.randint(0, 1))
+	# tet = str( input( "Do you want to have fun? ( y or n) "))
+	# if tet == "n":
+	# 	print(':(')
+	# elif  tet == "y":
+	# 	for i in range(9999999999):	
+	# 		print( random.randint(0, 1))
+	# else:
+	# 	print("FuCk you!!!")
--- testind.py
+++ testind.py
@@ -1,6 +1,5 @@
-import random
-
-# print us p 
+##BUGGG
+# print as p 
 num_1 = float(input ("Entet first num: "))
 num_2 = float(input ("Entet second num: "))
 res1 = float(num_1 + num_2)
@@ -9,6 +8,3 @@ res2 = float(num_1 + num_2 + res1)
 print ("Result is two", res2)
 res = float(res1 + res2)
 print ("Sum of results", res) 
-num_3 = int(input ("Entet  third num: "))
-for i in range (num_3):
-	print( random.randint(0, 1))
