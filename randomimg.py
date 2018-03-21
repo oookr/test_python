a49516032b81a1a9e2f4baed251bf8d7828abbb7
oookr <32430440+oookr@users.noreply.github.com>
2018-03-21 23:21:00 +0300

    Upgrade randomimg.py +

--- randomimg.py
+++ randomimg.py
@@ -2,7 +2,7 @@ import random as r
 # №1
 print ("How many numbers?")
 num = int(input ()) 
-if num > 100: # стереть int()  поставить if int(num) > 100:
+if num > 100: # стереть num = int(input ()) и поставить if int(num) > 100:
 	print ("Number >  100, enter the number < 100 ")
 	while num > 100 :
 		if num > 100:
@@ -41,37 +41,4 @@ elif tet != "y" and tet != "n":
 					print( r.randint(0, 1))					
 	pass	
 else:
-	print("Error/Ошибка (report it/сообщите о ней)")		
-
-# tet = str(input("Do you want to have fun? (y or n) "))
-# if tet == "n":
-# 	print(':(')
-# elif  tet == "y" "":
-
-# 	for i in range(9999999999):	
-# 		print( r.randint(0, 1))
-# else:
-# 	while tet != "n":
-# 		tet = str(input("Do you want to have fun? (y or n) "))
-# 		if tet == "n":
-# 			print(':(')
-# 		elif  tet == "y" "":
-# 			for i in range(9999999999):	
-# 				print( r.randint(0, 1))
-# 	while tet != "y":
-# 		# pass
-# 		tet = str(input("Do you want to have fun? (y or n) "))
-# 		if tet == "n":
-# 			print(':(')
-# 		elif  tet == "y" "":
-# 			for i in range(9999999999):	
-# 				print( r.randint(0, 1))
-
-	# tet = str( input( "Do you want to have fun? ( y or n) "))
-	# if tet == "n":
-	# 	print(':(')
-	# elif  tet == "y":
-	# 	for i in range(9999999999):	
-	# 		print( random.randint(0, 1))
-	# else:
-	# 	print("oh noo!!!")
+	print("Error/Ошибка (report it/сообщите о ней)")
