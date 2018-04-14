12e0a88f2ca4c868bafeae49684c9ba1896af4fe
oookr <32430440+oookr@users.noreply.github.com>
2018-04-14 21:38:40 +0300

    Upgrede import_my

---
+++ import_my.py
@@ -0,0 +1,23 @@
+import sys 
+
+def check_int(s):
+	try:
+		return int(s)
+	except:
+		print('please enter an integer!')
+		sys.exit()
+
+def check_str(x):
+	try:
+		return int(x) and float(x)
+	except:
+		print('please enter an string!')
+		sys.exit()
+
+def check_num(nu):
+	try:
+		return 1000	> nu
+	except:
+		print ('Number >  100, enter the number < 100')
+		sys.exit()
+		
