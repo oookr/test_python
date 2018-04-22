65e3d04fdb4faed775663d694b2db675093add49
oookr <32430440+oookr@users.noreply.github.com>
2018-04-22 23:09:12 +0300

    Upgrade testind.py

--- testind.py
+++ testind.py
@@ -17,15 +17,22 @@ from import_my import check_int
 #2
 #The game guess what a word
 # check_num = ["1","2","3","4","5","6","7","8","9","0"]
-a = input ("Enter the word: ")	
-if ("mam") in a or ("Mam") in a  or ("Мам")in a or ("мам")in a:
-	mamka = r'start chrome file:///D:/small_work/python_test/img/dumtss.jpg'
+
+a = input ("Enter the word: ")
+
+if ("mam") in a or ("Mam") in a or ("Мам")in a or ("мам")in a or ("mother") in a:
+	mamka = r'start chrome file:///D:/small_work/python_test/img/dumtss.jpg'	
 	#this program assumes that you have a chrome
 	subprocess.Popen(mamka,shell = True)
 	sys.exit()
 elif ("1") in a or ("2") in a or ("3") in a or ("4") in a or ("5") in a or ("6") in a or ("7") in a or ("8") in a or ("9") in a or ("0") in a :
 	print("Hey what's up?")
 	# while :
+	sys.exit()
+elif ("google") in a or ("google.com") in a:
+	google = r'start chrome /new-tab'	
+	#this program assumes that you have a chrome
+	subprocess.Popen(google,shell = True)
 	sys.exit()
 b = input ("What kind of letter do you need?")
 if (":") in b:
