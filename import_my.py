6d041ade82e6a8279d0a044cea29dfd3cbe4bfb8
oookr <32430440+oookr@users.noreply.github.com>
2018-06-19 07:21:03 +0300

    Update import_my.py

--- import_my.py
+++ import_my.py
@@ -16,6 +16,17 @@ def check_str(x):
 		print('please enter an string!')
 		sys.exit()
 
+def check_str2(x):
+	try:
+		# nam = (1,2,3,4,5,6,7,8,9,0)
+		nam_str= ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
+		# nam_all =nam + nam_str
+		# return x != 0 or x != 1 or x != 2 or x != 3 or x != 4 or x != 5 or x != 6 or x != 7 or x != 8 or x != 9 or x != "0" or x != "1" or x != "2" or x != "3" or x != "4" or x != "5" or x != "6" or x != "7" or x != "8" or x != "9"
+		return x != nam_str
+	except:
+		print('please enter an string!')
+		sys.exit()
+
 def check_num(num):
 	try:
 		return 1000	> num
