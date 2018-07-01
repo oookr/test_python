99c3012b0b36c599c30d2629e545ff722bdf0b65
oookr <32430440+oookr@users.noreply.github.com>
2018-07-01 13:35:14 +0300

    Update tac_tic_toe.py

--- tac_tic_toe.py
+++ tac_tic_toe.py
@@ -48,30 +48,30 @@ text = Label(tk,text="double-click if won",font=('Times 10 bold'), bg="#0D1829",
 text.grid()
 
 button1 = Button(tk,text=' ',font=('Times 15 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button1))
-button1.grid(row=1,column=0)
+button1.grid(row=1,column=0,sticky = S+N+E+W)
 
 button2 = Button(tk,text=' ',font=('Times 15 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button2))
-button2.grid(row=1,column=1)
+button2.grid(row=1,column=1,sticky = S+N+E+W)
 
 button3 = Button(tk,text=' ',font=('Times 15 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button3))
-button3.grid(row=1,column=2)
+button3.grid(row=1,column=2,sticky = S+N+E+W)
 
 button4 = Button(tk,text=' ',font=('Times 15 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button4))
-button4.grid(row=2,column=0)
+button4.grid(row=2,column=0,sticky = S+N+E+W)
 
 button5 = Button(tk,text=' ',font=('Times 15 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button5))
-button5.grid(row=2,column=1)
+button5.grid(row=2,column=1,sticky = S+N+E+W)
 
 button6 = Button(tk,text=' ',font=('Times 15 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button6))
-button6.grid(row=2,column=2)
+button6.grid(row=2,column=2,sticky = S+N+E+W)
 
 button7 = Button(tk,text=' ',font=('Times 15 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button7))
-button7.grid(row=3,column=0)
+button7.grid(row=3,column=0,sticky = S+N+E+W)
 
 button8 = Button(tk,text=' ',font=('Times 15 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button8))
-button8.grid(row=3,column=1)
+button8.grid(row=3,column=1,sticky = S+N+E+W)
 
 button9 = Button(tk,text=' ',font=('Times 15 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button9))
-button9.grid(row=3,column=2)
+button9.grid(row=3,column=2,sticky = S+N+E+W)
 
 tk.mainloop()
