40b787a6a1d255a3f23543a2c4a2767021198f67
oookr <32430440+oookr@users.noreply.github.com>
2018-07-15 03:03:46 +0300

    Update randoming.py

--- randoming.py
+++ randoming.py
@@ -1,57 +1,17 @@
 import random as r
-import tkinter
-from import_my import check_int
-from import_my import check_str
+from tkinter import*
 
-# №1
-print ("How many numbers?")
-num_str = input()
-num = check_int(num_str)
+tk=Tk()
+tk.title("Randoming")
+tk.geometry("400x300")
 
-if num > 100: # стереть num = int(input ()) и поставить if int(num) > 100: (str)	
-	while num > 100 :
-		if num > 100:
-			num = input ("Number >  100, enter the number < 100\nHow many numbers?\n")
-			num = check_int(num)
-		if num <= 100:	
-			print ("Your result:")
-			for i in range(num):
-				print( r.randint(0, 1))
-			print ("\n___Good___\n")
-	pass
+efrom = Entry(tk,)
+efrom.grid(row =1, column = 0)
+eto= Entry(tk,)
+eto.grid(row =0, column = 1)
 
-elif num <= 100:
-	print ("Your result:")
-	for i in range(num):	
-		
-		print( r.randint(0, 1))
-	print ("\n___Good___\n")
+btn = Button(tk, text="generate", height=10,width= 20)
+btn.grid(row = 1,column=0)
+#  r.randint("from", "to")
 
-else:
-	print("Error/Ошибка") 
-
-
-# №2
-tet = input("\nDo you want to have fun? (y or n) ")
-tet = check_str(tet) 
-
-if tet == "n":
-	print(':(')
-elif  tet == "y":
-	while tet == "y" :
-		for i in range(1):	
-			print( r.randint(0, 1))
-
-elif tet != "y" and tet != "n": 
-	while tet != "y" and tet != "n":
-		tet = str(input("|y or n| "))
-		if tet == "n":
-			print(':(')
-		while tet == "y":
-			for i in range(1): #!!!STOP program "Ctrl + C"!!!
-				print( r.randint(0, 1))					
-	pass	
-else:
-	print("Error/Ошибка")
-
-
+tk.mainloop()
