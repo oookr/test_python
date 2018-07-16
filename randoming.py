24a7f538fff4109f902a504a927fff6fc8a2b1fd
oookr <32430440+oookr@users.noreply.github.com>
2018-07-17 01:20:32 +0300

    End randoming.py

--- randoming.py
+++ randoming.py
@@ -1,17 +1,6 @@
 import random as r
-from tkinter import*
-
-tk=Tk()
-tk.title("Randoming")
-tk.geometry("400x300")
-
-efrom = Entry(tk,)
-efrom.grid(row =1, column = 0)
-eto= Entry(tk,)
-eto.grid(row =0, column = 1)
-
-btn = Button(tk, text="generate", height=10,width= 20)
-btn.grid(row = 1,column=0)
-#  r.randint("from", "to")
-
-tk.mainloop()
+frm=input("from \n")
+frm = int(frm)
+to = input("to \n")
+to = int(to)
+print("Result is\n",r.randint(frm, to))
