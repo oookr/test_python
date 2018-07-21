acb1951b57fd5be1f28c4171a0ad9307f926cfe4
oookr <32430440+oookr@users.noreply.github.com>
2018-07-21 03:13:28 +0300

    Update randoming.py

--- randoming.py
+++ randoming.py
@@ -7,32 +7,40 @@ tk.geometry()
 
 tops = Frame(tk, width = 50, height= 6,bd=4 ,bg="#85929E" , relief="raise")
 tops.pack(side=TOP)
-
-# def res
-
-def btnEqual():
-	global operator
-	r.randint("from", "to")
-	operator = res
-	messagebox.showinfo("Result",res)
-
-operator = ""
 Input_from=StringVar()
 Input_to=StringVar()
+# def res
+c =Label(tops, text ="from:",bg="#85929E",)
+c.grid(row=1, column=0)
+to=Label(tops, text ="to  :",bg="#85929E",)
+to.grid(row=2, column=0)
+ec = Entry(tops, bg="#34495E",textvariable=Input_from)
+ec.grid(row =1, column = 1)
+eto = Entry(tops, bg="#34495E",textvariable=Input_to)
+eto.grid(row =2, column = 1)
+
+def btnEqual():
+	ec = int(ec)
+	eto = int(eto) 
+	res = r.randint(c, to)
+	res = str(res)
+	tk.messagebox.showinfo("Result", res)
+
 # Input_res=StringVar()
 
 
 
-efrom = Entry(tops, bg="#34495E",textvariable=Input_from)
-efrom.grid(row =1, column = 0)
-to= Entry(tops, bg="#34495E",textvariable=Input_to)
-to.grid(row =1, column = 1)
-
-# res= Entry(tk, bg="red",textvariable=Input_res)
-# res.grid(row =2, column = 0)
 
 
-btn = Button(tops, text="generate", command=lambda:btnEqual(), height=8,width= 30, bg="#3498DB")
+btn = Button(tops, text="generate", command=lambda:btnEqual(), height=4,width= 15, bg="#3498db")
 btn.grid(row = 3,columnspan=4)
 
 tk.mainloop()
+
+'''
+import random as r
+s = input("from : ")
+ss= input("to   : ")
+res = r.randint(int(s), int(ss))
+print (res)
+'''
