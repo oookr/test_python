801e9c8c89fdd743146765402937b7ec789ef6c9
oookr <32430440+oookr@users.noreply.github.com>
2019-01-07 20:34:58 +0300

    download in working

--- working/download.py
+++ working/download.py
@@ -1,23 +1,29 @@
 def ttt(buttons):
 	import urllib.request
+	
 	if button1:
-		path = 'https://www.keralatourism.org/images/picture/large'
-		name = 'Munnar_the_magical_experience_of_nature_77.jpg'
+		path = 'https://www.btwin.com/en/img'
+		name = 'cover.jpg'
 		if '.' in name: 
-			url = path + '/' + name 
+			url = path + '/' + name
+			# from random import randint
+			# rand = randint(1, 99)
+			# name = str(rand) + name 
 			urllib.request.urlretrieve(url, name) 
 			end('1')
-		else: #Don't worck
-			url = path + '.html' 
+		else:
+			url = 'https://www.btwin.com/en/'
 			urllib.request.urlretrieve(url, 'samthing.html')
 			end('2') 
 def end(self):
 	from sys import exit
+
 	text = 'Beginning file download with urllib...\n' + self
 	exit(text)
 if __name__ == '__main__':
 	from tkinter import *
+
 	tk = Tk()
 	button1 = Button(tk,text='download',font=('Times 20 bold'),bg='gray',fg='white',height=4,width=8,command=lambda:ttt(button1))
 	button1.grid(row=1,column=1)
-	tk.mainloop()	
+	tk.mainloop()
