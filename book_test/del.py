c96319f038ba7c87b6383c8dee7f5be785683eae
oookr <32430440+oookr@users.noreply.github.com>
2019-01-13 18:46:02 +0300

    Update del.py

--- book_test/del.py
+++ book_test/del.py
@@ -1,18 +1,18 @@
 import os
 print(" List directory given by path(" + os.getcwd() + "):\n", os.listdir("."), end="\n")
 
-file = input("The name of the file you want to delete: ")
+file = input('The name of the file you want to delete: ')
 
 # Is there a file in the directory?
-while os.listdir(file):
+while file in os.listdir("."):
 	# if the file is, then the file is deleted:
 	try:
 		os.remove(file)
 	# elif the file is not there:
 	except:
 		qesten = input("Continue yes or no: ")
-		if qesten in "n":
+		if "n" in qesten:
 			os.exit()
-		elif qesten in "y":
+		elif "y" in qesten:
 			file = input("The name of the file you want to delete: ")
 			os.remove(file)
