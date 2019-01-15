8c3887ac4c8d597331d634234538c5be47f3622f
oookr <32430440+oookr@users.noreply.github.com>
2019-01-15 21:35:52 +0300

    Update coroutines.py

--- book_test/coroutines.py
+++ book_test/coroutines.py
@@ -4,3 +4,17 @@ def print_matches(matchtext):
         line = (yield)
         if matchtext in line:
             print(line)
+
+matchers = [
+    print_matches("python"),
+    print_matches("guido"),
+    print_matches("jython")
+]
+
+for m in matchers:
+    m.__next__()
+
+wwwlog = tail(open("access-log"))
+for line in wwwlog:
+    for m in matchers:
+        m.send(line)
