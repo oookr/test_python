0c5e520ab8ca5e6ece7323292e7f901484657712
oookr <32430440+oookr@users.noreply.github.com>
2019-01-22 21:57:50 +0300

    Update coroutines.py

--- book_test/coroutines.py
+++ book_test/coroutines.py
@@ -1,31 +1,19 @@
-def print_matches(matchtext):
-    print("Поиск подстроки", matchtext)
+import time
+
+def tail(f):
+    f.seek(0, 2)
     while True:
-        line = (yield)
-        if matchtext in line:
-            print(line)
+        line = f.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
 
-matchers = [
-    print_matches("python"),
-    print_matches("guido"),
-    print_matches("jython")
-]
+def grep(lines,seartchtext):
+    for line in lines:
+        if seartchtext in line: yield line
 
-for m in matchers:
-    m.__next__()
-
-wwwlog = tail(open("access-log"))
-for line in wwwlog:
-    for m in matchers:
-        m.send(line)
-
-
-# import time
-# def tail(f):
-#     f.seek(0, 2)
-#     while True:
-#         line = f.readline()
-#         if not line:
-#             time.sleep(0.1)
-#             continue
-#         yield line
+wwwlog = tail(open("test.py"))
+pylines = grep(wwwlog, "python")
+for line in pylines:
+    print(line, end=" ")
